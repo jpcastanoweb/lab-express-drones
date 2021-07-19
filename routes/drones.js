7320dc5203873a413ad96374095c18ab9a6a62b5
Juan Pablo Castano <jpcastanom@gmail.com>
2021-07-19 18:32:48 -0500

    finished

--- routes/drones.js
+++ routes/drones.js
@@ -38,17 +38,40 @@ router.post("/drones/create", (req, res, next) => {
 
 router.get("/drones/:id/edit", (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  // const { id } = req.params
+  Drone.findById(req.params.id)
+    .then((droneFound) => {
+      console.log(droneFound)
+      res.render("drones/update-form", {
+        drone: droneFound,
+      })
+    })
+    .catch((e) => console.log(e))
 })
 
 router.post("/drones/:id/edit", (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const { id } = req.params
+  const { name, propellers, maxSpeed } = req.body
+  Drone.findByIdAndUpdate(id, { name, propellers, maxSpeed })
+    .then(() => {
+      res.redirect("/drones")
+    })
+    .catch((e) => {
+      console.log("Error happened: ", e)
+      res.redirect(`/drones/${id}/edit`)
+    })
 })
 
 router.post("/drones/:id/delete", (req, res, next) => {
   // Iteration #5: Delete the drone
-  // ... your code here
+  const { id } = req.params
+
+  Drone.findByIdAndDelete(id)
+    .then(() => {
+      res.redirect("/drones")
+    })
+    .catch((e) => console.log(e))
 })
 
 module.exports = router
